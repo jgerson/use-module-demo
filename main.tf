c2a3ea382e8f7702d1018e2af3190edef2f6495f
Jeremy Gerson <jgerson@hashicorp.com>
2018-08-07 10:12:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
-module "website" {
-  source       = "app.terraform.io/jgersonorg1/basewebsite/azurerm"
+//--------------------------------------------------------------------
+// Variables
+variable "basewebsite_env" {}
+
+//--------------------------------------------------------------------
+// Modules
+module "basewebsite" {
+  source  = "app.terraform.io/jgersonorg1/basewebsite/azurerm"
+  version = "1.0"
+
   azure_region = "eastus"
-  env          = "jeremy"
-  size         = "medium"
+  env = "${var.basewebsite_env}"
+  size = "medium"
 }
 
 output "ip_addresses" {
